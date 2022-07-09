1352d4e7c321f3b1c0231a2097e25357c5c41033
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-09 18:54:39 +0700

    copy stuff

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverBuildTarget: 'netlify',
+  server: './server.js',
   ignoredRouteFiles: ['**/.*'],
   // https://remix.run/docs/en/v1/pages/gotchas#importing-esm-packages
   serverDependenciesToBundle: ['lodash-es', 'p-memoize', 'mimic-fn'],
