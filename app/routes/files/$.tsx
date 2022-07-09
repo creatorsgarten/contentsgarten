972a56162212732444b3d58920960b736d882fed
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-09 17:46:31 +0700

    add auth module from directcommit

---
+++ app/routes/files/$.tsx
@@ -0,0 +1,3 @@
+export default function WikiFile() {
+  return <div className="p-8">…</div>
+}
---
+++ app/wiki-engine/types.ts
@@ -0,0 +1,16 @@
+/**
+ * A wiki page represents a rendered wiki page.
+ * It can be backed by a file, or generated dynamically.
+ * If it is backed by a file, it can be edited.
+ */
+export interface WikiPage {
+  file?: WikiFileRef;
+}
+
+/**
+ * A wiki file represents a file that is part of a wiki.
+ */
+export interface WikiFileRef {
+  path: string;
+  sha?: string;
+}
