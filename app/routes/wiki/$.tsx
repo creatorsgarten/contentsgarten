32db9c242a1436b0b80e62de550214040882debd
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-11 02:46:58 +0700

    API-first

--- app/routes/wiki/$.tsx
+++ app/routes/wiki/$.tsx
@@ -1,92 +1,11 @@
-import type {
-  ActionFunction,
-  LoaderFunction,
-  MetaFunction,
-} from '@remix-run/node'
-import { json } from '@remix-run/node'
+import type { MetaFunction } from '@remix-run/node'
 import { Form, Link, useLoaderData } from '@remix-run/react'
 import type { FC } from 'react'
 import { Markdown } from '~/markdown'
-import type { WikiPage } from '~/wiki-engine'
-import { WikiActor } from '~/wiki-engine/actor'
 
-interface LoaderData {
-  edit?: WikiPageEdit
-  view?: {
-    page: WikiPage
-    editPath?: string
-  }
-  pageTitle: string
-}
-
-interface WikiPageEdit {
-  formTarget: string
-  gitHubEditPath?: string
-  content: string
-  sha?: string
-}
-
-export const action: ActionFunction = async ({ request, params }) => {
-  const actor = await WikiActor.fromRequest(request)
-  const slug = params['*'] as string
-  const formData = await request.formData()
-  const result = await actor.updatePage(slug, {
-    content: formData.get('content') as string,
-    sha: formData.get('sha') as string | undefined,
-  })
-  return json({
-    result,
-  })
-}
-
-export const loader: LoaderFunction = async ({ request, params }) => {
-  const actor = await WikiActor.fromRequest(request)
-  const slug = params['*'] as string
-  if (!slug) {
-    return new Response('', {
-      status: 302,
-      headers: {
-        Location: '/wiki/MainPage',
-      },
-    })
-  }
-  const searchParams = new URL(request.url).searchParams
-  const page = await actor.getPage(slug)
-  const error = (message: string): LoaderData => ({
-    pageTitle: slug,
-    view: {
-      page: {
-        content: '**An error has occurred.** ' + message,
-        path: slug,
-      },
-    },
-  })
-  const result = await (async (): Promise<LoaderData> => {
-    if (searchParams.get('action') === 'edit' && page.file) {
-      const file = await actor.getFile(page.file.path)
-      return {
-        pageTitle: `${file.found ? 'Editing' : 'Creating'} ${slug}`,
-        edit: {
-          formTarget: `/wiki/${page.path}`,
-          gitHubEditPath: `https://github.dev/creatorsgarten/contentsgarten-wiki/blob/main/${page.file.path}`,
-          content: file.found
-            ? Buffer.from(file.content, 'base64').toString()
-            : '',
-          sha: file.found ? file.sha : undefined,
-        },
-      }
-    } else {
-      return {
-        pageTitle: slug,
-        view: {
-          page,
-          editPath: `/wiki/${page.path}?action=edit`,
-        },
-      }
-    }
-  })()
-  return json(result)
-}
+import type { LoaderData, WikiPageEdit } from '~/routes/api/wiki/$'
+import { action, loader } from '~/routes/api/wiki/$'
+export { action, loader }
 
 export const meta: MetaFunction = ({ data, params }) => {
   const { pageTitle } = data as LoaderData
