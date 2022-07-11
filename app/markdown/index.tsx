95245a6b10667d34c65ef132596248bb59b7e3ef
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-11 14:08:44 +0700

    Liquid support

--- app/markdown/index.tsx
+++ app/markdown/index.tsx
@@ -1,4 +1,5 @@
 import MarkdownIt from 'markdown-it'
+import container from 'markdown-it-container'
 import type { FC } from 'react'
 
 export interface Markdown {
@@ -6,6 +7,28 @@ export interface Markdown {
 }
 
 export const Markdown: FC<Markdown> = (props) => {
-  const html = MarkdownIt({ html: true }).render(props.text)
+  const md = MarkdownIt({ html: true })
+  addContainer(md, 'lead', {
+    open: '<div class="lead">',
+    close: '</div>',
+  })
+  const html = md.render(props.text)
   return <div dangerouslySetInnerHTML={{ __html: html }} />
+}
+
+function addContainer(md: MarkdownIt, name: string, options: ContainerOptions) {
+  md.use(container, name, {
+    render(tokens: any[], idx: number) {
+      if (tokens[idx].nesting === 1) {
+        return options.open
+      } else {
+        return options.close
+      }
+    },
+  })
+}
+
+interface ContainerOptions {
+  open: string
+  close: string
 }
