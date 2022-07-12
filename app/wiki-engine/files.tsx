0b4ea410cf1cf3443a9875cbfcc504017515ff67
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-12 20:31:11 +0700

    add fs caching

--- app/wiki-engine/files.tsx
+++ app/wiki-engine/files.tsx
@@ -1,7 +1,13 @@
 import { once } from 'lodash-es'
 import { App, Octokit } from 'octokit'
 import pMemoize from 'p-memoize'
-import type { WikiContext } from './types'
+import type {
+  GetFileResult,
+  PutFileOptions,
+  PutFileResult,
+  WikiContext,
+  WikiFileSystem,
+} from './types'
 
 const getRepoConfig = once(() => {
   const [owner, repo] = (process.env.GH_REPO as string).split('/')
@@ -38,97 +44,102 @@ const getOctokit = pMemoize(async () => {
   )
 })
 
-async function getFile(
-  context: WikiContext,
-  path: string,
-): Promise<GetFileResult> {
-  context.writeDiagnosticLog(`Fetching file ${path}`)
-  const octokit = await getOctokit()
-  const { owner, repo } = getRepoConfig()
-  try {
-    const { data } = await octokit.rest.repos.getContent({
+const createGitHubFileSystem = (): WikiFileSystem => {
+  return { getFile, putFile }
+
+  async function getFile(
+    context: WikiContext,
+    path: string,
+  ): Promise<GetFileResult> {
+    context.writeDiagnosticLog(`Fetching file ${path}`)
+    const octokit = await getOctokit()
+    const { owner, repo } = getRepoConfig()
+    try {
+      const { data } = await octokit.rest.repos.getContent({
+        owner,
+        repo,
+        path,
+      })
+      if (!('content' in data)) {
+        throw new Error('No content found')
+      }
+      context.writeDiagnosticLog(`Fetched ${path}`)
+      return {
+        found: true,
+        content: data.content,
+        sha: data.sha,
+      }
+    } catch (error) {
+      context.writeDiagnosticLog(`Fetch failed for ${path}: ${error}`)
+      if (isApiError(error) && error.status === 404) {
+        return { found: false }
+      }
+      throw error
+    }
+  }
+
+  async function putFile(
+    context: WikiContext,
+    path: string,
+    options: PutFileOptions,
+  ): Promise<PutFileResult> {
+    const octokit = await getOctokit()
+    const { owner, repo } = getRepoConfig()
+    const { data } = await octokit.rest.repos.createOrUpdateFileContents({
       owner,
       repo,
       path,
+      sha: options.sha,
+      content: options.content,
+      message:
+        options.message +
+        (options.userId
+          ? `\n\n\nCo-authored-by: User <${options.userId}+username@users.noreply.github.com>`
+          : ''),
     })
     if (!('content' in data)) {
       throw new Error('No content found')
     }
-    context.writeDiagnosticLog(`Fetched ${path}`)
     return {
-      found: true,
-      content: data.content,
-      sha: data.sha,
+      sha: data.content!.sha!,
     }
-  } catch (error) {
-    context.writeDiagnosticLog(`Fetch failed for ${path}: ${error}`)
-    if (isApiError(error) && error.status === 404) {
-      return { found: false }
-    }
-    throw error
   }
 }
 
-async function putFile(
-  context: WikiContext,
-  path: string,
-  options: PutFileOptions,
-): Promise<PutFileResult> {
-  const octokit = await getOctokit()
-  const { owner, repo } = getRepoConfig()
-  const { data } = await octokit.rest.repos.createOrUpdateFileContents({
-    owner,
-    repo,
-    path,
-    sha: options.sha,
-    content: options.content,
-    message:
-      options.message +
-      (options.userId
-        ? `\n\n\nCo-authored-by: User <${options.userId}+username@users.noreply.github.com>`
-        : ''),
-  })
-  if (!('content' in data)) {
-    throw new Error('No content found')
-  }
+const createCachedFileSystem = (base: WikiFileSystem): WikiFileSystem => {
+  const cache = new Map()
+  const memoizedGet = pMemoize(
+    (context: WikiContext, path: string, miss: () => void) => {
+      miss()
+      return base.getFile(context, path)
+    },
+    {
+      cache,
+      cacheKey: (args) => args[1],
+    },
+  )
   return {
-    sha: data.content!.sha!,
+    getFile: (context, path) => {
+      let missed = false
+      const promise = memoizedGet(context, path, () => (missed = true))
+      if (!missed) {
+        context.writeDiagnosticLog(`Cache hit for ${path}`)
+      }
+      return promise
+    },
+    putFile: async (context, path, options) => {
+      try {
+        return await base.putFile(context, path, options)
+      } finally {
+        cache.clear()
+      }
+    },
   }
 }
 
-export type GetFileResult = GetFileResultFound | GetFileResultNotFound
-
-export interface GetFileResultFound {
-  found: true
-  content: string
-  sha: string
-}
-
-export interface GetFileResultNotFound {
-  found: false
-}
-
-interface PutFileOptions {
-  content: string
-  message: string
-  userId?: number
-  sha?: string
-}
-
-export interface PutFileResult {
-  sha: string
-}
-
-export interface WikiFileSystem {
-  getFile: (context: WikiContext, path: string) => Promise<GetFileResult>
-  putFile: (
-    context: WikiContext,
-    path: string,
-    options: PutFileOptions,
-  ) => Promise<PutFileResult>
-}
-
-export const defaultFileSystem: WikiFileSystem = { getFile, putFile }
+export const defaultFileSystem: WikiFileSystem = createCachedFileSystem(
+  createGitHubFileSystem(),
+)
 
 function isApiError(e: unknown): e is ApiError {
   return 'status' in (e as object) && 'response' in (e as object)
