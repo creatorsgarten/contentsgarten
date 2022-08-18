46bc7ce9e4e65b36720a2a5e0fef8ba56c7a5610
dtinth on MBP M1 <dtinth@spacet.me>
2022-08-18 16:50:16 +0700

    Cache public key for 300s only

--- src/packlets/wiki-auth/firebase.ts
+++ src/packlets/wiki-auth/firebase.ts
@@ -3,13 +3,17 @@ import pMemoize from 'p-memoize'
 import axios from 'axios'
 import { has } from 'lodash-es'
 import { createPublicKey } from 'crypto'
+import ExpiryMap from 'expiry-map'
 
-const getPublicKeys = pMemoize(async () => {
-  const response = await axios.get(
-    'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com',
-  )
-  return response.data
-})
+const getPublicKeys = pMemoize(
+  async () => {
+    const response = await axios.get(
+      'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com',
+    )
+    return response.data
+  },
+  { cache: new ExpiryMap(300000) },
+)
 
 const getPublicKey = pMemoize(async (kid) => {
   const publicKeys = await getPublicKeys()
