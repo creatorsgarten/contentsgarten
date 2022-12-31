7733d625c364956ebeb1a31670af5a03839933fc
dtinth on MBP M1 <dtinth@spacet.me>
2023-01-01 03:35:57 +0700

    Pass storage and auth to context

--- src/packlets/contentsgarden/ContentsgartenContext.ts
+++ src/packlets/contentsgarden/ContentsgartenContext.ts
@@ -1,5 +1,9 @@
 import type { QueryClient } from '@tanstack/query-core'
+import type { ContentsgartenStorage } from './ContentsgartenStorage'
+import type { ContentsgartenAuth } from './ContentsgartenAuth'
 
 export interface ContentsgartenContext {
   queryClient: QueryClient
+  storage: ContentsgartenStorage
+  auth: ContentsgartenAuth
 }
