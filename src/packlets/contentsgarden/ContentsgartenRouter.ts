b9d645e5b29e8ff5af2564a3073fa332060d4308
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-04 02:19:54 +0700

    Add GetPageResult

--- src/packlets/contentsgarden/ContentsgartenRouter.ts
+++ src/packlets/contentsgarden/ContentsgartenRouter.ts
@@ -28,17 +28,32 @@ async function getPage(ctx: ContentsgartenContext, pageRef: string) {
   const filePath = pageRefToFilePath(ctx, pageRef)
   const engine = createLiquidEngine(ctx)
   const file = await ctx.config.storage.getFile(ctx, filePath)
-  return {
+  const result: GetPageResult = {
     pageRef,
     title: pageRef,
     file: {
       path: filePath,
       revision: file ? file.revision : undefined,
+      content: file ? file.content.toString('utf8') : '',
     },
     content: String(
       file
         ? await engine.renderFile(pageRef)
         : '(This page currently does not exist)',
     ),
+    status: file ? 200 : 404,
   }
+  return result
+}
+
+export interface GetPageResult {
+  status: 200 | 404
+  pageRef: string
+  title: string
+  file?: {
+    path: string
+    content: string
+    revision?: string
+  }
+  content: string
 }
