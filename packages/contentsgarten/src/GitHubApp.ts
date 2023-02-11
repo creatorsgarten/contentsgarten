689b6c485a0ce7f2cc22ebfa2c1ffdf4b33a5bc2
Thai Pangsakulyanont <dtinth@spacet.me>
2023-02-11 21:20:51 +0000

    ????

--- packages/contentsgarten/src/GitHubApp.ts
+++ packages/contentsgarten/src/GitHubApp.ts
@@ -1,5 +1,15 @@
+import { createPrivateKey } from 'crypto'
+
 export class GitHubApp {
-  constructor(public config: GitHubAppConfig) {}
+  constructor(public config: GitHubAppConfig) {
+    if (/BEGIN RSA PRIVATE KEY/.test(config.privateKey)) {
+      const pkcs8 = createPrivateKey(config.privateKey).export({
+        type: 'pkcs8',
+        format: 'pem',
+      })
+      this.config = { ...config, privateKey: String(pkcs8) }
+    }
+  }
 }
 
 export interface GitHubAppConfig {
