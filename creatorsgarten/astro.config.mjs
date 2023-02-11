4c53bc32c28fcb8832383d5c99d86e22b336f7b9
Thai Pangsakulyanont <dtinth@spacet.me>
2023-02-11 12:07:38 +0000

    7 files changed, 137 insertions(+), 49 deletions(-)

--- creatorsgarten/astro.config.mjs
+++ creatorsgarten/astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import node from '@astrojs/node';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [tailwind()]
+});
