fac78a763567716e845e9649e34355b0079c42ce
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-12 01:41:42 +0700

    4 files changed, 43 insertions(+), 3 deletions(-)

--- creatorsgarten/astro.config.mjs
+++ creatorsgarten/astro.config.mjs
@@ -7,10 +7,13 @@ import node from '@astrojs/node';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
