0b6273aefc7005e6252590745e3ace3553f956b7
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-12 02:19:09 +0700

    12 files changed, 260 insertions(+), 84 deletions(-)

--- creatorsgarten/tailwind.config.cjs
+++ creatorsgarten/tailwind.config.cjs
@@ -1,8 +1,27 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      typography: {
+        lg: {
+          css: {
+            lineHeight: '1.555555',
+          },
+        },
+        DEFAULT: {
+          css: {
+            '--tw-prose-links': colors.sky[600],
+            a: {
+              textDecoration: 'none',
+              fontWeight: 'inherit',
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
 }
