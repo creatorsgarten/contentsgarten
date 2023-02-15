dde57dd3dc1fc824bd4663b9d212c89210470cf4
dtinth on MBP M1 <dtinth@spacet.me>
2023-02-15 22:44:38 +0700

    Add edit icon

--- creatorsgarten/src/components/WikiPage.tsx
+++ creatorsgarten/src/components/WikiPage.tsx
@@ -3,6 +3,7 @@ import type { GetPageResult } from 'contentsgarten'
 import type { FC } from 'react'
 import { TrpcProvider, trpc } from 'src/utils/trpc'
 import { clsx } from 'clsx'
+import { Icon } from 'react-iconify-icon-wrapper'
 
 export interface WikiPage {
   page: GetPageResult
@@ -23,12 +24,25 @@ export const WikiPageInner: FC<WikiPage> = (props) => {
   })
   const page = freshPageQuery.data ?? stalePage
   return (
-    <Markdown
-      className={clsx(
-        'prose-h1:text-4xl prose-h1:mt-12 prose-h1:font-medium',
-        freshPageQuery.data && freshPageQuery.isRefetching && 'opacity-50',
-      )}
-      text={page.content}
-    />
+    <>
+      <h1>
+        {page.title}
+        {!!page.file && (
+          <span
+            className="text-xl pl-2"
+            onClick={() => alert('under construction!')}
+          >
+            <Icon icon="mdi:lead-pencil" />
+          </span>
+        )}
+      </h1>
+      <Markdown
+        className={clsx(
+          'prose-h1:text-4xl prose-h1:mt-12 prose-h1:font-medium',
+          freshPageQuery.data && freshPageQuery.isRefetching && 'opacity-50',
+        )}
+        text={page.content}
+      />
+    </>
   )
 }
